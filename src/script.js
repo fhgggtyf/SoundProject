289205b94fb9481984cddb86b5a72fa4fb3e7f44
fhgggtyf <xjc18502795710@outlook.com>
2023-11-12 01:14:04 +0400

    commit

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   let currentScreen = 'start-screen';
-  const character = document.createElement('div');
+  const character = document.createElement('img');
   character.id = 'character';
-  character.style.left = '0px';
+  character.style.left = '1300px';
+  character.src = "img/character pngs/right.png";
   document.getElementById(currentScreen).appendChild(character);
 
   const screenBounds = {
-    'start-screen': { right: 'main-hall', leftLimit: 0, rightLimit: 200 }, // Example limits
-    'main-hall': { right: 'hallway-1', leftLimit: 0, rightLimit: 300 },
-    'hallway-1': { right: 'hallway-2', up: 'room1.html', leftLimit: 0, rightLimit: 300 },
-    'hallway-2': { right: 'exit', up: 'room2.html', leftLimit: 0, rightLimit: 300 },
+    'start-screen': { right: 'main-hall', leftLimit: 1300, rightLimit: 1520 }, // Example limits
+    'main-hall': { right: 'hallway', leftLimit: 350, rightLimit: 1300 },
+    'hallway': { right: 'exit', up1: 'room1.html', up2: 'room2.html', leftLimit: 350, rightLimit: 1400 },
     'exit': { right: 'start-screen', leftLimit: 0, rightLimit: 200 }
   };
 
@@ -31,12 +31,53 @@ document.addEventListener('DOMContentLoaded', () => {
         characterPos -= moveAmount;
         character.style.left = `${characterPos}px`;
       }
-    } else if (keyName === 'ArrowUp') {
-      if (screenBounds[currentScreen].up) {
-        window.location.href = screenBounds[currentScreen].up;
+    } else if (keyName === 'ArrowUp' && screenBounds[currentScreen].right == 'exit') {
+      if (parseInt(character.style.left) <= 850) {
+        window.location.href = screenBounds[currentScreen].up1;
+      }
+      else {
+        window.location.href = screenBounds[currentScreen].up2;
       }
     }
   });
+
+  var observer = new MutationObserver(function (mutations) {
+    // Check each mutation in the list
+    mutations.forEach(function (mutation) {
+      if (mutation.attributeName === 'style') {
+        // The 'style' attribute has changed
+        var newLeftValue = parseInt(character.style.left) || 0;
+
+        if (screenBounds[currentScreen].right == 'hallway') {
+          if(newLeftValue==260){
+            alert();
+          }
+          else if (newLeftValue == 1000) {
+            alert();
+          }
+          else if (newLeftValue == 1280) {
+            alert();
+          }
+        }
+        else if (screenBounds[currentScreen].right == 'exit') {
+          if(newLeftValue==460){
+            alert();
+          }
+          else if (newLeftValue == 1000) {
+            alert();
+          }
+          else if (newLeftValue == 1280) {
+            alert();
+          }
+        }
+
+      }
+    });
+  });
+
+  var config = { attributes: true, subtree: false, attributeFilter: ['style'] };
+
+  observer.observe(character, config);
 
   function navigateTo(nextScreen) {
     if (nextScreen.startsWith('/')) {
@@ -50,7 +91,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (currentScreenEl && nextScreenEl) {
       currentScreenEl.removeChild(character);
       nextScreenEl.appendChild(character);
-      character.style.left = '0px'; // Reset character position in new screen
+      character.style.left = '250px'; // Reset character position in new screen
       currentScreenEl.classList.remove('active');
       nextScreenEl.classList.add('active');
       currentScreen = nextScreen;
